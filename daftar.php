3d6f75aa9d19e29c246c477da5993f0489f1b7f0
AhmadMauludin <ahmadmaldin1799@gmail.com>
2024-10-04 11:15:34 +0700

    sdfdsf

--- daftar.php
+++ daftar.php
@@ -30,11 +30,11 @@
             <P><label>Id </label></P>
             <input type="text" name="id" class="form_login" placeholder="Masukan id" required="required">
             <p><label>Username</label>
-            <P><input type="text" name="nama pokdarwis" class="form_login" placeholder="Masukan Nama Pokdarwis" required="required"></P>
+            <P><input type="text" name="username" class="form_login" placeholder="Masukan Nama Pokdarwis" required="required"></P>
             <p><label>Alamat</label>
-            <P><input type="text" nama="alamat pokdarwis" class="form_login" placeholder="Masukan Alamat Pokdarwis" required="required"></P>
+            <P><input type="text" name="alamat" class="form_login" placeholder="Masukan Alamat Pokdarwis" required="required"></P>
             <p><label>Potensi</label>
-            <P><input type="text" nama="potensi wisata" class="form_login" placeholder="Masukan Potensi wisata" required="required"></P>
+            <P><input type="text" name="potensi" class="form_login" placeholder="Masukan Potensi wisata" required="required"></P>
             <label>Password</label>
             <p><input type="password" name="password" class="form_login" placeholder="Masukkan Password" required="required"></p>
             <p><input type="hidden" name="level" class="form_login" placeholder="Masukan id" value="User" required="required"></P>
--- proses-daftar.php
+++ proses-daftar.php
@@ -5,12 +5,12 @@ require_once "config/database.php";
 if (isset($_POST['simpan'])) {
     $id            = $_POST['id'];
     $username      = mysqli_real_escape_string($db, trim($_POST['username']));
-    $alamat      = mysqli_real_escape_string($db, trim($_POST['alamat pokdarwis']));
-    $potensi       = mysqli_real_escape_string($db, trim($_POST['potensi wisata']));
+    $alamat      = mysqli_real_escape_string($db, trim($_POST['alamat']));
+    $potensi       = mysqli_real_escape_string($db, trim($_POST['potensi']));
     $password      = mysqli_real_escape_string($db, trim($_POST['password']));
 
     // perintah query untuk menyimpan data ke tabel is_siswa
-    $query = mysqli_query($db, "INSERT INTO user (id, username, alamat pokdarwis, potensi wisata, password) VALUES('$id','$username', '$alamat pokdarwis', '$potensi  wisata', '$password')");
+    $query = mysqli_query($db, "INSERT INTO daftar (id, username, alamat, potensi, password) VALUES('$id','$username', '$alamat', '$potensi', '$password')");
 
     // cek hasil query
     if ($query) {
