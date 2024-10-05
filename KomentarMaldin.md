e483a67a01bbf05e71478757596163364ac87a4e
AhmadMauludin <ahmadmaldin1799@gmail.com>
2024-10-05 14:16:46 +0700

    fgd

--- KomentarMaldin.md
+++
@@ -1,1 +0,0 @@
-Oke selamat bekerja
--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// mengaktifkan session php
-session_start();
-
-// menghapus semua session
-session_destroy();
-
-// mengalihkan halaman ke halaman login
-header("location:login.php");
